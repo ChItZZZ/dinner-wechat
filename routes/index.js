fd234eaa3d65573760c0d66661a1174241d53af7
ChItZZZ <my@mmmorningdembp.qad.com>
2016-08-24 17:26:05 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/order', function (req, res, next) {
+  res.render('order');
+})
 
 module.exports = router;
