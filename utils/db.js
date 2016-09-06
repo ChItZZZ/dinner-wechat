147ac4a89f50701d630a9db66e9c11db4d44dca7
ChItZZZ <648267970@qq.com>
2016-09-07 00:11:38 +0800

    add db.js(crud)

---
+++ utils/db.js
@@ -0,0 +1,132 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '115.159.94.41',
+    user: 'remote',
+    password: 'remote',
+    database: 'modu'
+});
+
+//增
+exports.create = function (table, arr) {
+    connection.connect(function (err) {
+        if (err) {
+            console.log('[query] - :' + err);
+            return;
+        }
+        console.log('[connection connect]  succeed!');
+    });
+    var userAddSql = 'INSERT INTO stf_mst(stf_name,stf_password,stf_code) VALUES(?,?,?)';
+    var userAddSql_Params = ['Nicky', '123456', 3];
+    connection.query(userAddSql, userAddSql_Params, function (err, result) {
+        if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('--------------------------INSERT----------------------------');
+        //console.log('INSERT ID:',result.insertId);
+        console.log('INSERT ID:', result);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+
+    connection.end(function (err) {
+        if (err) {
+            return;
+        }
+        console.log('[connection end] succeed!');
+    });
+}
+
+//改
+exports.update = function () {
+
+    connection.connect(function (err) {
+        if (err) {
+            console.log('[query] - :' + err);
+            return;
+        }
+        console.log('[connection connect]  succeed!');
+    })
+
+    var userModSql = 'UPDATE stf_mst SET stf_name = ?,stf_password = ? WHERE stf_id = ?';
+    var userModSql_Params = ['hahaha', '123123', 3];
+    connection.query(userModSql, userModSql_Params, function (err, result) {
+        if (err) {
+            console.log('[UPDATE ERROR] - ', err.message);
+            return;
+        }
+        console.log('--------------------------UPDATE----------------------------');
+        console.log('UPDATE affectedRows', result.affectedRows);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+
+    connection.end(function (err) {
+        if (err) {
+            return;
+        }
+        console.log('[connection end] succeed!');
+    })
+
+}
+
+//查
+exports.select = function () {
+    connection.connect(function (err) {
+        if (err) {
+            console.log('[query] - :' + err);
+            return;
+        }
+        console.log('[connection connect]  succeed!');
+    })
+
+    var userGetSql = 'SELECT * FROM stf_mst';
+    connection.query(userGetSql, function (err, result) {
+        if (err) {
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('--------------------------SELECT----------------------------');
+        console.log(result);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+
+    connection.end(function (err) {
+        if (err) {
+            return;
+        }
+        console.log('[connection end] succeed!');
+    })
+
+}
+//删
+exports.delete = function () {
+
+    connection.connect(function (err) {
+        if (err) {
+            console.log('[query] - :' + err);
+            return;
+        }
+        console.log('[connection connect]  succeed!');
+    })
+
+    var userDelSql = 'DELETE FROM userinfo';
+    connection.query(userDelSql, function (err, result) {
+        if (err) {
+            console.log('[DELETE ERROR] - ', err.message);
+            return;
+        }
+
+        console.log('--------------------------DELETE----------------------------');
+        console.log('DELETE affectedRows', result.affectedRows);
+        console.log('-----------------------------------------------------------------\n\n');
+    });
+
+    connection.end(function (err) {
+        if (err) {
+            return;
+        }
+        console.log('[connection end] succeed!');
+    })
+
+}
