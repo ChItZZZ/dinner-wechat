c9b1b0c34b29e7d29a016b90b3abb7dc98c1b041
ZhangShangyu <shangyu_zhang@outlook.com>
2016-09-07 22:57:12 +0800

    "db exec and demo"

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var iG = require('../models/items');
+var item = require('../controller/goods')
 
 var db = require('../utils/db');
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-router.get('/order', function (req, res, next) {
+router.get('/order', function(req, res, next) {
     res.render('order');
 })
 
-router.get('/demo', function (req, res, next) {
-    res.render('file');
+
+//database operation demo
+router.get('/demo', function(req, res, next) {
+
+    item.demo(function(err, result) {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.send(result);
+        res.end();
+    })
+
 })
 
-router.get('/items', function (req, res, next) {
+router.get('/items', function(req, res, next) {
     //console.log(typeof iG.items);
     res.json(iG);
     //res.send('hello world');
 })
-router.get('/test', function (req, res, next) {
+
+router.get('/test', function(req, res, next) {
     res.write('hello world');
     res.write('again');
     res.end();
 })
 
-router.get('/user', function (req, res, next) {
+router.get('/user', function(req, res, next) {
     db.select();
     res.end();
 })
