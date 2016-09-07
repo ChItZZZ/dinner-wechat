a3f323701edca774afcd415ce963337a5fb29285
ZhangShangyu <shangyu_zhang@outlook.com>
2016-09-07 10:24:44 +0800

    git test zsy

--- controller/goods.js
+++ controller/goods.js
@@ -2,10 +2,10 @@
  * Created by my on 9/7/16.
  */
 var db = require('../utils/db');
-exports.listItems = function () {
-    var sql = "SELECT * FROM stf_mst";
+exports.listItems = function(userid) {
+    var sql = "SELECT * FROM stf_mst ";
     db.select(sql);
     //......
 }
 
-
+// git test zsy
