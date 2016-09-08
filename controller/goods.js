95668b2cf2024fc4ddaf24145c00141ec1ecc48d
ZhangShangyu <shangyu_zhang@outlook.com>
2016-09-08 20:58:13 +0800

    "get menus information from db,send to front end"

--- controller/goods.js
+++ controller/goods.js
@@ -20,6 +20,61 @@ exports.demo = function(callback) {
 
 }
 
-exports.listOrder = function() {
-    var sql = "SELECT * FROM ";
+/**
+ * get items information from db , callbacl error or json result
+ *    result json format like this:
+ *         items: {
+ *               "面类": [{id: "1", name: "咸肉面", cls: "肉品", price: "15", sels: "45", imageUrl: "images/1.jpg"}],
+ *
+ *               "蔬菜": [{id: "13", name: "飘菜", cls: "蔬菜", price: "152", sels: "524", imageUrl: "images/13.jpg"},
+ *                       {id: "14", name: "苦瓜", cls: "蔬菜", price: "154", sels: "524", imageUrl: "images/14.jpg"},],
+ * 
+ *               "糕点": [{id: "24", name: "糕点", cls: "糕点", price: "152", sels: "12", imageUrl: "images/24.jpg"},
+ *                       {id: "25", name: "糕点", cls: "糕点", price: "154", sels: "16", imageUrl: "images/25.jpg"},
+ *                       {id: "26", name: "糕点", cls: "糕点", price: "151", sels: "18", imageUrl: "images/26.jpg"}]    
+ *              }
+ */
+exports.getItems = function(callback) {
+    var sql = "select b.* from gd_ctlg a right join gd_mst b " +
+        "on a.ctlg_name = b.gd_catalogue_name order by a.ctlg_name";
+    var values = [];
+    db.exec(sql, values, function(err, result) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        var items = {};
+
+        if (result.length > 0) {
+            var item = {};
+            var arr = [];
+
+            for (var i = 0; i < result.length; ++i) {
+                item['id'] = result[i].gd_id;
+                item['name'] = result[i].gd_name;
+                item['cls'] = result[i].gd_catalogue_name;
+                item['price'] = result[i].gd_price;
+                item['sels'] = result[i].gd_sales;
+                item['imageUrl'] = result[i].gd_picture;
+                arr.push(item);
+                item = {};
+                while (i < result.length - 1 && result[i].gd_catalogue_name == result[i + 1].gd_catalogue_name) {
+                    item['id'] = result[i + 1].gd_id;
+                    item['name'] = result[i + 1].gd_name;
+                    item['cls'] = result[i + 1].gd_catalogue_name;
+                    item['price'] = result[i + 1].gd_price;
+                    item['sels'] = result[i + 1].gd_sales;
+                    item['imageUrl'] = result[i + 1].gd_picture;
+                    arr.push(item);
+                    item = {};
+                    ++i;
+                }
+                items[result[i].gd_catalogue_name] = arr;
+                console.log(arr);
+                arr = [];
+            }
+        }
+
+        callback(null, items);
+    });
 }
