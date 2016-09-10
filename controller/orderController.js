63d39c97c58a42b4e659f5234dd0d7ec3a0cc1cd
ChItZZZ <648267970@qq.com>
2016-09-11 01:23:36 +0800

    modify order data format

--- controller/orderController.js
+++ controller/orderController.js
@@ -4,66 +4,64 @@
 var db = require('../utils/db');
 var sd = require('silly-datetime');
 
-exports.createOrder = function (req,res,next) {
+exports.createOrder = function (req, res, next) {
     console.log(req.body);
     //var data = JSON.parse(req.body);
     var data = req.body;
-    var userOpenId = data.wechatopenid;
-    var time=sd.format(new Date(), 'YYYY/MM/DD');
-    var store_id = parseInt(data.store_id);
-    var desk_id = parseInt(data.desk_id);
-    var foods = data.foods;
-    var price = 0;
-    var string = "";
-    for(var i = 0;i<foods.length;i++){
-        price = price + foods[i].food_price * foods[i].food_quantity;
-        string = string + foods[i].food_name+"*"+foods[i].food_quantity +";";
-    }
-    var values_order = [store_id, desk_id,time,userOpenId,0,price,string];
+    var order_str = data.order_str;
 
-    console.log(typeof (time));
+    var userOpenId = data.open_id;
+    var time = sd.format(new Date(), 'YYYY/MM/DD');
+    var store_id = parseInt(data.store_id || 1);
+    var desk_id = parseInt(data.desk_id || 1);
+    var order_obj = JSON.parse(data.order_str);
+    var price = data.price;
+    var string = '';
+
+    for (var i in order_obj) {
+        string += order_obj[i].name + "*" + order_obj[i].counter + ";";
+    }
+
+    var values_order = [store_id, desk_id, time, userOpenId, 0, price, string];
+
     var sql_order = 'INSERT INTO od_hdr (od_store_id,od_desk_id,od_date,od_wechatopenid,od_state,od_total_price,od_string) ' +
         'VALUES (?,?,?,?,?,?,?)';
-    db.exec(sql_order, values_order, function(err, result) {
+    db.exec(sql_order, values_order, function (err, result) {
         if (err) {
-            //callback(err);
             return;
         }
-        //callback(null, result);
         console.log(result.insertId);
         var order_id = result.insertId;
-        for(var i = 0;i<foods.length;i++){
+        for (var i in order_obj) {
             var sql_food = 'INSERT INTO od_ln (od_id,od_line_number,gd_name,gd_quantity,od_price) ' +
                 'VALUES (?,?,?,?,?)';
-           // var food_id = foods[i].food_id;
-            var food_name = foods[i].food_name;
-            var food_quantity= foods[i].food_quantity;
-            var food_price = foods[i].food_price;
-            var values_food = [order_id, i+1,food_name,food_quantity,food_price];
-            db.exec(sql_food,values_food, function(err, result) {
+            // var food_id = order_obj[i].food_id;
+            var food_name = order_obj[i].name;
+            var food_quantity = order_obj[i].counter;
+            var food_price = order_obj[i].price;
+            var values_food = [order_id, i + 1, food_name, food_quantity, food_price];
+            db.exec(sql_food, values_food, function (err, result) {
                 if (err) {
                     //callback(err);
                     return;
-                }else {
+                } else {
                     console.log("food inserted");
                 }
             });
         }
-
-
     });
     res.end();
 
 }
 
-exports.searchOrder = function (req,res,next) {
+exports.searchOrder = function (req, res, next) {
     console.log(req.body);
     //var data = JSON.parse(req.body);
     var data = req.body;
-    var userOpenId = data.wechatopenid;
+    var userOpenId = data.open_id;
     var values_order = [userOpenId];
     var sql_order = 'SELECT * FROM od_hdr where od_wechatopenid = ? ';
-    db.exec(sql_order, values_order, function(err, result) {
+    db.exec(sql_order, values_order, function (err, result) {
         if (err) {
             //callback(err);
             return;
