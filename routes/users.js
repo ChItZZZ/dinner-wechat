e0be1f88f3b438ee4c0308f78cc9f1516d636448
HulkTang <lawliethk@outlook.com>
2016-09-22 22:59:14 +0800

    userInfoTest

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const getToken = require('../websdk/getWebToken');
+const getUserInfo = require('../websdk/getWebUserInfo');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/userInfoTest', function (req, res) {
+  getToken(req.query.code)
+      .then(function (data) {
+        return JSON.parse(data);
+      })
+      .then(function (data) {
+        getUserInfo(data['access_token'], data['openid']).then(function _(){
+          res.render('userInfoTest.html', {userinfo: _});
+        })
+      });
 });
 
 module.exports = router;
