b3654c69a12ca1db6982f4c3e6e96638b54678c2
ZhangShangyu <shangyu_zhang@outlook.com>
2016-09-25 16:47:39 +0800

    add payment code

---
+++ payment/webhooks_verify.js
@@ -0,0 +1,35 @@
+'use strict';
+// Ping++ Server SDK
+
+
+var crypto = require("crypto"),
+    fs  = require("fs");
+
+
+// 验证 webhooks 签名方法：
+// raw_data：Ping++ 请求 body 的原始数据即 event ，不能格式化；
+// signature：Ping++ 请求 header 中的 x-pingplusplus-signature 对应的 value 值；
+// pub_key_path：读取你保存的 Ping++ 公钥的路径；
+// pub_key_contents：Ping++ 公钥，获取路径：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Ping++ 公钥
+
+
+// 验证 webhooks 签名
+var verify_signature = function(raw_data, signature, pub_key_path) {
+  var verifier = crypto.createVerify('RSA-SHA256').update(raw_data, "utf8");
+  var pub_key = fs.readFileSync(pub_key_path, "utf8");
+  return verifier.verify(pub_key, signature, 'base64');
+}
+
+// POST 原始请求数据是待验签数据，请根据实际情况获取
+var raw_data = '{"id":"evt_eYa58Wd44Glerl8AgfYfd1sL","created":1434368075,"livemode":true,"type":"charge.succeeded","data":{"object":{"id":"ch_bq9IHKnn6GnLzsS0swOujr4x","object":"charge","created":1434368069,"livemode":true,"paid":true,"refunded":false,"app":"app_vcPcqDeS88ixrPlu","channel":"wx","order_no":"2015d019f7cf6c0d","client_ip":"140.227.22.72","amount":100,"amount_settle":0,"currency":"cny","subject":"An Apple","body":"A Big Red Apple","extra":{},"time_paid":1434368074,"time_expire":1434455469,"time_settle":null,"transaction_no":"1014400031201506150354653857","refunds":{"object":"list","url":"/v1/charges/ch_bq9IHKnn6GnLzsS0swOujr4x/refunds","has_more":false,"data":[]},"amount_refunded":0,"failure_code":null,"failure_msg":null,"metadata":{},"credential":{},"description":null}},"object":"event","pending_webhooks":0,"request":"iar_Xc2SGjrbdmT0eeKWeCsvLhbL"}';
+// 签名在头部信息的 X-Pingplusplus-Signature 字段，
+// 需要对字段名做个根据你自己的规则格式化处理，否则可能会取不到
+var signature = 'BX5sToHUzPSJvAfXqhtJicsuPjt3yvq804PguzLnMruCSvZ4C7xYS4trdg1blJPh26eeK/P2QfCCHpWKedsRS3bPKkjAvugnMKs+3Zs1k+PshAiZsET4sWPGNnf1E89Kh7/2XMa1mgbXtHt7zPNC4kamTqUL/QmEVI8LJNq7C9P3LR03kK2szJDhPzkWPgRyY2YpD2eq1aCJm0bkX9mBWTZdSYFhKt3vuM1Qjp5PWXk0tN5h9dNFqpisihK7XboB81poER2SmnZ8PIslzWu2iULM7VWxmEDA70JKBJFweqLCFBHRszA8Nt3AXF0z5qe61oH1oSUmtPwNhdQQ2G5X3g==';
+// Ping++ 公钥，获取路径：登录 [Dashboard](https://dashboard.pingxx.com)->点击管理平台右上角公司名称->开发信息-> Ping++ 公钥
+var pub_key_path = __dirname + "/pingpp_rsa_public_key.pem";
+
+if (verify_signature(raw_data, signature, pub_key_path)) {
+  console.log('verification succeeded');
+} else {
+  console.log('verification failed');
+}
