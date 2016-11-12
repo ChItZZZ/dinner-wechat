589545cb4e3c9a3caf390856d38f2bb1b7fcb5d1
qiancs1101 <qian.cs@outlook.com>
2016-11-12 22:03:55 +0800

    food configuration

--- controller/itemController.js
+++ controller/itemController.js
@@ -21,5 +21,25 @@ exports.scanQR = function (req,res,next) {
     itemList.push(itemId);
     req.session.orderItems = itemList;
     res.redirect('/');
+};
+
+
+exports.getConfiguration = function (req,res,next) {
+    var data = req.query;
+    var itemId = data.item_id;
+    var value = [itemId];
+
+    item.getConfig(value, function (err, result) {
+        if (err) {
+            //callback(err);
+            return;
+        }
+        //callback(null, result);
+        console.log(result);
+        res.json(result);
+        res.end();
+    });
 }
+
+
 
