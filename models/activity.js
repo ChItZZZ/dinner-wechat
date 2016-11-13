10eda79dab8368fbe24c78f3a2dfac89b6a30134
HulkTang <lawliethk@outlook.com>
2016-11-13 19:04:24 +0800

    activity and recruit entity

---
+++ models/activity.js
@@ -0,0 +1,31 @@
+var db = require("../utils/db");
+var sd = require('silly-datetime');
+
+exports.inquire = function(callback){
+    var activityInquire = "select * from activity_config where activity_end_date >= sysdate order by activity_type";
+    var values = [];
+    db.exec(activityInquire, values, function (err, result) {
+        var activities = {};
+        if (err) {
+            callback(err);
+            return;
+        }
+        else{
+            var activity = {};
+            for(var i = 0; i < result.length; i += 1){
+                activity['id'] = result[i].activity_id;
+                activity['type'] = result[i].activity_type;
+                activity['amount1'] = result[i].activity_amount1;
+                activity['amount2'] = result[i].activity_amount2;
+                activity['amount3'] = result[i].activity_amount3;
+                activity['amount4'] = result[i].activity_amount4;
+                activity['startDate'] = result[i].activity_start_date;
+                activity['endDate'] = result[i].activity_end_date;
+                activity['description'] = result[i].activity_description;
+                activity['catalogue'] = result[i].activity_catalogue;
+                activities[i] = activity;
+            }
+        }
+        callback(null, activities);
+    });
+};
