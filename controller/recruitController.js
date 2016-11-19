829bfc947b095eb1f4f7db496ef7e7e761d36b5f
HulkTang <lawliethk@outlook.com>
2016-11-19 16:27:55 +0800

    Rename js file

--- controller/recruitController.js
+++ controller/recruitController.js
@@ -3,10 +3,10 @@ var recruit = require("../models/recruit");
 exports.inquire = function(req, res, next){
     console.log('info: ' + 'in inquire');
     var data = req.body;
-    console.log('info: ' + 'recruit err 1');
+    console.log('info: ' + 'recruit.js err 1');
     recurit.inquire(function (err, result) {
         if (err) {
-            console.log('info: ' + 'in recruit inquire controller err1');
+            console.log('info: ' + 'in recruit.js inquire controller err1');
             res.end();
             return;
         }
--- models/recruit.js
+++ models/recruit.js
@@ -2,21 +2,21 @@ var db = require("../utils/db");
 var sd = require('silly-datetime');
 
 exports.inquire = function(callback){
-    console.log('info: ' + 'recruit error 1');
+    console.log('info: ' + 'recruit.js error 1');
     var recruitInquire = "select * from recruit_master ";
-    console.log('info: ' + 'recruit error 2');
+    console.log('info: ' + 'recruit.js error 2');
     var values = [];
-    console.log('info: ' + 'recruit error 3');
+    console.log('info: ' + 'recruit.js error 3');
     var recruits = {};
-    console.log('info: ' + 'recruit error 4');
+    console.log('info: ' + 'recruit.js error 4');
     db.exec(recruitInquire, values, function (err, result) {
         if (err) {
-            console.log('info: ' + 'in recruit inquire err1');
+            console.log('info: ' + 'in recruit.js inquire err1');
             callback(err);
             return;
         }
         else{
-            console.log('info: ' + 'in recruit inquire else');
+            console.log('info: ' + 'in recruit.js inquire else');
             var recruit = {};
             for(var i = 0; i < result.length; i += 1){
                 recuit['id'] = result[i].recruit_id;
