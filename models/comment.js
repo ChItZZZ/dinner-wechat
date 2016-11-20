16460e2c0056b84eb75638c2850382619a858016
HulkTang <lawliethk@outlook.com>
2016-11-20 11:13:25 +0800

    Add comment entity.

---
+++ models/comment.js
@@ -0,0 +1,20 @@
+var db = require("../utils/db");
+var sd = require('silly-datetime');
+
+exports.add = function (openid, content, callback) {
+    var commentInsert = "INSERT INTO comment_matser (comment_openid,comment_content,comment_date)" +
+        "VALUES(?,?,?)";
+    var time = sd.format(new Date(), 'YYYY/MM/DD/hh:mm');
+    var insertValues = [openid,content,time];
+    db.exec(commentInsert, insertValues, function (err, result) {
+        var addCommentResult = {};
+        if(err){
+            addCommentResult['successful'] = '0';
+            callback(err, addCommentResult);
+            return;
+        }
+        addCommentResult['successful'] = '1';
+        callback(err, addCommentResult);
+        return;
+    });
+};
