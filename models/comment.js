8446bdfc48b40eff9ec80fc54e927467541d34c7
HulkTang <lawliethk@outlook.com>
2016-11-23 21:21:09 +0800

    Add phone number and name to comment record.

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,11 @@
 var db = require("../utils/db");
 var sd = require('silly-datetime');
 
-exports.add = function (openid, content, callback) {
-    var commentInsert = "INSERT INTO comment_matser (comment_openid,comment_content,comment_date)" +
-        "VALUES(?,?,?)";
+exports.add = function (openid, content, name, phone, callback) {
+    var commentInsert = "INSERT INTO comment_matser (comment_openid,comment_content,comment_date,comment_name,comment_phone)" +
+        "VALUES(?,?,?,?,?)";
     var time = sd.format(new Date(), 'YYYY/MM/DD/hh:mm');
-    var insertValues = [openid,content,time];
+    var insertValues = [openid,content,time,name,phone];
     db.exec(commentInsert, insertValues, function (err, result) {
         var addCommentResult = {};
         if(err){
