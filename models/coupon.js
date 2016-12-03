d9c2aba004535f43fdcc618a7347a467421ad6ce
Bennett Qian <qian.cs@outlook.com>
2016-12-03 16:55:10 +0800

    no message

--- models/coupon.js
+++ models/coupon.js
@@ -6,6 +6,7 @@ var db = require('../utils/db');
 
 exports.updateCoupon = function(value,callback){
     var sql = 'UPDATE coupon_master SET coupon_number = coupon_number - 1 where coupon_master_id = ?';
+    var sql_delete = 'delete from coupon_master where coupon_number = 0 and coupon_master_id = ?';
     var values = [value];
     db.exec(sql, values, function(err, result) {
         if (err) {
@@ -14,6 +15,13 @@ exports.updateCoupon = function(value,callback){
         }
         callback(null, result);
     });
+    db.exec(sql_delete, values, function (err, result) {
+        if(err){
+            //callback(err);
+            return;
+        }
+        //callback(null, result);
+    })
 };
 
 exports.rollback = function(value,callback){
