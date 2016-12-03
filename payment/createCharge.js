f6b1e6364ccb39030abd1b5559f47f9bbb515f06
ZhangShangyu <shangyu_zhang@outlook.com>
2016-12-03 23:10:14 +0800

    fix

--- payment/createCharge.js
+++ payment/createCharge.js
@@ -68,64 +68,6 @@ exports.createNew = function (req,res,next)
               
 }
 
-exports.create = function (req,res,next) 
-{
-  
-  pingpp.parseHeaders(req.headers); // 把从客户端传上来的 Headers 传到这里
-  // 设置你的私钥路径，用于请求的签名，对应的公钥请填写到 Ping++ 管理平台
-  pingpp.setPrivateKeyPath(__dirname + "/your_rsa_private_key.pem");
-  req.setEncoding('utf-8');
-  var data = req.body;
-  data.open_id  = req.body.openId || '123';
-  console.log('session id ' + req.session.openid);
-  var channel = data.channel;
-  var openid = data.open_id;
-  var amount = data.amount;
-  var client_ip = req.connection.remoteAddress;
-  var extra = {};
-  switch (channel) {
-    case 'alipay_wap':
-      extra = {
-        'success_url': 'http://wechat.qiancs.cn',
-        'cancel_url': 'http://wechat.qiancs.cn'
-      };
-      break;
-    case 'wx_pub':
-      extra = {
-        'open_id': openid
-      };
-      break;
-  }
-
-  orderController.createOrderInfo(data,function(err,order_id){
-      if(err){
-	console.log('err' + err);
-        res.end();
-        return;
-      }
-      pingpp.charges.create({
-      order_no:  order_id,
-      app:       {id: APP_ID},
-      channel:   channel,
-      amount:    amount,
-      client_ip: client_ip,
-      currency:  "cny",
-      subject:   "Charge Subject",
-      body:      "Charge Body",
-      extra:     extra
-      }, function(err,charge){
-      if(err){
-        console.log("生成charge失败" + err);
-        res.end();
-        return;
-      }
-      console.log('生成charge成功');
-      res.send(charge);
-      res.end();
-    });
-  });  
-              
-}
 
 exports.createForUnfinishedOrder = function (req,res,next)
 {
