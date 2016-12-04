45dbb3f56bdb4ab2ae29de3f22eee71044f71c61
Bennett Qian <qian.cs@outlook.com>
2016-12-04 21:21:15 +0800

    delete invalid couopn

--- models/coupon.js
+++ models/coupon.js
@@ -6,7 +6,7 @@ var db = require('../utils/db');
 
 exports.updateCoupon = function(value,callback){
     var sql = 'UPDATE coupon_master SET coupon_number = coupon_number - 1 where coupon_master_id = ?';
-    var sql_delete = 'delete from coupon_master where coupon_number = 0 and coupon_master_id = ?';
+    var sql_delete = 'delete from coupon_master where coupon_number < 1 and coupon_master_id = ?';
     var values = [value];
     db.exec(sql, values, function(err, result) {
         if (err) {
