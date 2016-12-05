d167cfc7bfc4a3c48ea3be1187f021643bd72586
Bennett Qian <qian.cs@outlook.com>
2016-12-05 17:30:22 +0800

    return only valid coupons

--- controller/couponController.js
+++ controller/couponController.js
@@ -36,34 +36,35 @@ exports.getCoupons = function(req,res,next){
                     //callback(null, result);
                     console.log(detail);
                     if(detail.length>0){
-                        coupon_detail['id']=coupon.coupon_master_id;
-                        coupon_detail['start_date']=coupon.coupon_start_date;
-                        coupon_detail['end_date']=coupon.coupon_end_date;
-                        coupon_detail['number']=coupon.coupon_number;
                         if(today<coupon.coupon_start_date){   // 未生效
-                            coupon_detail['status']= 0;
+                            //coupon_detail['status']= 0;
                         }else if(today>coupon.coupon_end_date || coupon.coupon_number < 1){  //已过期
-                            coupon_detail['status']= 1;
+                            //coupon_detail['status']= 1;
                         }else {
                             coupon_detail['status']= 2;
-                        }
-                        switch(detail[0].coupon_type) {
-                            case 1:
-                                coupon_detail['condition']=detail[0].coupon_amount1;
-                                coupon_detail['discount']=detail[0].coupon_amount2;
+                            coupon_detail['id']=coupon.coupon_master_id;
+                            coupon_detail['start_date']=coupon.coupon_start_date;
+                            coupon_detail['end_date']=coupon.coupon_end_date;
+                            coupon_detail['number']=coupon.coupon_number;
+                            switch(detail[0].coupon_type) {
+                                case 1:
+                                    coupon_detail['condition']=detail[0].coupon_amount1;
+                                    coupon_detail['discount']=detail[0].coupon_amount2;
 
-                                break;
-                            case 2:
-                                coupon_detail['discount']=detail[0].coupon_amount1;
-                                break;
-                            default:
-                                break;
+                                    break;
+                                case 2:
+                                    coupon_detail['discount']=detail[0].coupon_amount1;
+                                    break;
+                                default:
+                                    break;
+                            }
+                            coupon_detail['type'] = detail[0].coupon_type;
+                            coupon_detail['description']=detail[0].coupon_description;
+                            coupon_detail['catalogue']=detail[0].coupon_catalogue;
+
+                            coupon_list.push(coupon_detail);
+                            coupon_detail={};
                         }
-                        coupon_detail['type'] = detail[0].coupon_type;
-                        coupon_detail['description']=detail[0].coupon_description;
-                        coupon_detail['catalogue']=detail[0].coupon_catalogue;
-                        coupon_list.push(coupon_detail);
-                        coupon_detail={};
                         //console.log(coupon_list);
                         callback(null, !err)
                     }
